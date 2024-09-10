8e74d4a99316b7cdfcc1c5fd905b322a64895e0d
ST10334021 Kamogelo <ST10334021@imconnect.edu.za>
2024-09-10 18:30:31 +0200

    Adding Additional fields

---
+++ POE_Part1_Sem4/Claim_Submission.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POE_Part1_Sem4
+{
+    /// <summary>
+    /// Interaction logic for Claim_Submission.xaml
+    /// </summary>
+    public partial class Claim_Submission : Window
+    {
+        public Claim_Submission()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ POE_Part1_Sem4/Track_Progress.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POE_Part1_Sem4
+{
+    /// <summary>
+    /// Interaction logic for Track_Progress.xaml
+    /// </summary>
+    public partial class Track_Progress : Window
+    {
+        public Track_Progress()
+        {
+            InitializeComponent();
+        }
+    }
+}
